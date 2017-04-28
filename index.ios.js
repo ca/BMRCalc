4c4e1aa6e5c6391c58c4ce1a1251c5494905a432
Alex Vallorosi <alexvallorosi@gmail.com>
2017-04-28 16:06:55 -0400

    removed android & updated app

--- index.ios.js
+++ index.ios.js
@@ -1,53 +1,50 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { AppRegistry, Switch, Text, StyleSheet, TextInput, View } from 'react-native';
 
 export default class BMRCalc extends Component {
-  render() {
+  constructor (props) {
+    super(props);
+    this.state = {weight: 0, height: 0, age: 0, male: true};
+  }
+
+  render () {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
+      <View style={{padding: 10}}>
+        <Text style={{padding: 10, fontSize: 42, fontWeight: "bold", textAlign: "center", margin: 20, marginTop: 40}}>
+          { this.state.gender ? Math.round(10*this.state.weight*(0.453592) + 6.25*this.state.height*(2.54) - 5*this.state.age + 5) : Math.round(10*this.state.weight*(0.453592) + 6.25*this.state.height*(2.54) - 5*this.state.age - 161) } kcal
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
+        <TextInput
+          style={{height: 40, textAlign: "center"}}
+          placeholder="Enter your weight in pounds"
+          keyboardType="numeric"
+          onChangeText={(weight) => this.setState({weight})}
+        />
+        <TextInput
+          style={{height: 40, textAlign: "center"}}
+          placeholder="Enter your height in inches"
+          keyboardType="numeric"
+          onChangeText={(height) => this.setState({height})}
+        />
+        <TextInput
+          style={{height: 40, textAlign: "center"}}
+          placeholder="Enter your age in years"
+          keyboardType="numeric"
+          onChangeText={(age) => this.setState({age})}
+        />
+        <View style={{flexDirection: "row", justifyContent: "center", alignContent: "center", marginTop: 20}}>
+          <Text style={{ lineHeight: 31, fontWeight: "bold", fontSize: 20, color: "lightpink"}}>Female</Text>
+          <Switch
+            onValueChange={(gender) => this.setState({gender})}
+            style={{marginLeft: 10, marginRight: 10}}
+            value={this.state.gender} />
+          <Text style={{ lineHeight: 31, fontWeight: "bold", fontSize: 20, color: "powderblue"}}>Male</Text>
+        </View>
+        <Text style={{margin: 20, textAlign: "center", color: "gray"}}>
+          This calculator is based on the Mifflin St Jeor Equation for calculating Basal Metabolic Rate, which has been found to be the most accurate!
         </Text>
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('BMRCalc', () => BMRCalc);
